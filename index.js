1f0101cd7d3703e8b54b556cc715082034c62623
mussinbenarbia <mouhssine.benarbia@gmail.com>
2021-03-20 12:24:23 +0900

    implement mia complainer

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 function PrintSomethingInTheLog() {
-    console.log('Can you read this?');
+    console.log("Nyoooo! Some of your tests are failing!");
 }
+
+function miaComplains(){
+    const complains = [
+        "Nyoooo! Some of your tests are failing!",
+        "Delete system32 pls",
+        "Rule 'don't suck' violated: this is terrible",
+        "I will NOT merge this PR!",
+    ]
+    return complains[Math.floor(Math.random() * complains.length)]
+}
+
+document.querySelector("#mia_img").addEventListener("click", function(e){
+    document.querySelector("#mia_msg").innerText = miaComplains();
+})
