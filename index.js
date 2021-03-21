e4cbdfe8aa57d4d5cc6934a4e632a433cc1a6588
Evan Trujillo <evomatic@pop-os.localdomain>
2021-03-21 21:23:35 +0900

    changed picture to Emmet_Brickowski || changed to Emmet_Brickowski quotes || wrapped printSomethingInTheLog function in an IIFE

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-function PrintSomethingInTheLog() {
-    console.log("Nyoooo! Some of your tests are failing!");
-}
+(function PrintSomethingInTheLog() {
+    console.log("Everything is awesome!");
+})();
 
-function miaComplains(){
+function emmetAwesome(){
     const complains = [
-        "Nyoooo! Some of your tests are failing!",
-        "Delete system32 pls",
-        "Rule 'don't suck' violated: this is terrible",
-        "I will NOT merge this PR!",
+        "There's no signs or anything. How does anyone know what to do?",
+        "No, they're expecting us to show up in a Bat-Spaceship, or a pirate spaceship or a rainbow-sparkle spaceship",
+        "Wear clothes. Oop! I almost forgot that one!",
+        "Drink overpriced coffee!",
     ]
     return complains[Math.floor(Math.random() * complains.length)]
 }
 
-document.querySelector("#mia_img").addEventListener("click", function(e){
-    document.querySelector("#mia_msg").innerText = miaComplains();
+document.querySelector("#emmet_img").addEventListener("click", function(e){
+    document.querySelector("#emmet_msg").innerText = emmetAwesome();
 })
