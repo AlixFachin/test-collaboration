b74189b7228ba5a0a5ddbd4f2f044a8fd251800c
Alix Fachin <alix.fachin@me.com>
2021-03-22 16:23:06 +0900

    Merging the two suggested features

--- index.js
+++ index.js
@@ -14,4 +14,18 @@ function emmetAwesome(){
 
 document.querySelector("#emmet_img").addEventListener("click", function(e){
     document.querySelector("#emmet_msg").innerText = emmetAwesome();
-})
+});
+
+function miaComplains(){
+    const complains = [
+        "Nyoooo! Some of your tests are failing!",
+        "Delete system32 pls",
+        "Rule 'don't suck' violated: this is terrible",
+        "I will NOT merge this PR!",
+    ]
+    return complains[Math.floor(Math.random() * complains.length)]
+}
+
+document.querySelector("#mia_img").addEventListener("click", function(e){
+    document.querySelector("#mia_msg").innerText = miaComplains();
+});
